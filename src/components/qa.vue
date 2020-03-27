66b1874a02e4c0a9085be41ca203ba0b12b10f92
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-03-27 20:29:20 +0000

    Add qa component: makes block of question and answer component

---
+++ src/components/qa.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id='qa'>
+    <question :q='q'/>
+    <answer :a='a'/>
+  </div>
+</template>
+
+<script>
+import question from './question.vue'
+import answer from './answer.vue'
+
+export default {
+  name: 'qa',
+  components: {
+    question,
+    answer,
+  },
+  props: {
+    q: String,
+    a: String
+  }
+}
+</script>
+
+<style scoped>
+#qa{
+  --block-spacing: 7.5em;
+
+  margin-top: var(--block-spacing);
+  margin-bottom: var(--block-spacing);
+}
+</style>
