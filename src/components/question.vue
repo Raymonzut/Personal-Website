d9c4685b648f184f1883623fb74110a8c19c64a3
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-03-27 18:18:58 +0000

    Add question component; italic text div

---
+++ src/components/question.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='question_box'>
+    {{ q }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'question',
+  props: {
+    q: String
+  }
+}
+</script>
+
+<style scoped>
+#question_box {
+  font-style: italic;
+}
+</style>
