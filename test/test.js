4cd5285032cf9c5c8dcfe24105d995f7a904d013
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-03-28 22:04:05 +0000

    Remove app.js dummy code

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const app = require('../src/app');
-
-describe('App', () => {
-	it('should greet with Hello World!', () => {
-		assert.equal(app(), 'Hello World!');
-	});
-});
