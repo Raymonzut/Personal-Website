a0edd4d64e737542665324a1f8ef12d219ae80bd
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-03-28 22:08:14 +0000

    Add aboutme component with age

---
+++ src/components/aboutme.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ age }}
+  </div>
+</template>
+
+<script>
+import me from '../me.js'
+
+export default {
+  name: 'aboutme',
+  computed: {
+    age: me.age
+  }
+}
+</script>
