911764efc80562bc0ef955b0dd90bfd86e615906
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-04-17 22:50:16 +0200

    Add /api/posts/ route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+const posts = require('./routes/api/posts')
+
+app.use('/api/posts', posts)
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
