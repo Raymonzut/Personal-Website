5a9de291690e8a803f4390d9fc2425587068ad71
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-04-25 15:44:15 +0200

    Add validator for Post prop id

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -14,6 +14,10 @@ export default {
     id: {
       type: String,
       name: "id",
+      validator: val => {
+        const reg = /([0-9]|[a-f]){24}/
+        return reg.test(val)
+      },
     }
   }
 }
