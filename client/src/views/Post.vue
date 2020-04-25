f89c9251e69a06ef130ebf1e0b84bd7ba8b13f7f
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-04-25 14:38:40 +0200

    Add Post view displaying passed id

---
+++ client/src/views/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="Post">
+  <br>
+  Read other <a href="/posts">posts</a>
+  <br>
+  The id of the post is: {{ id }}
+  </div> 
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    id: {
+      type: String,
+      name: "id",
+    }
+  }
+}
+</script>
+
+<style scoped></style>
