e18bdd2e2979aa44383d7d4fd5cc47abc7bdc426
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-04-25 13:48:06 +0200

    Add /qa route for QAPage

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Posts from '../views/Posts.vue'
+import QAPage from '../views/QAPage'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/posts",
     name: "Posts",
     component: Posts,
+  },
+  {
+    path: "/qa",
+    name: "QA",
+    component: QAPage
   },
 ]
 
