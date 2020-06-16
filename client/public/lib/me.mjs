2c7f3278929558e957b5568a3a0c8ab70763954a
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-06-16 18:48:26 +0200

    Add me.mjs with age()

---
+++ client/public/lib/me.mjs
@@ -0,0 +1,17 @@
+export function age() {
+  let birthdate = new Date(2002, 8, 29)
+  let now = new Date()
+
+  let age = now.getFullYear() - birthdate.getFullYear()
+
+  if (now.getMonth() < birthdate.getMonth()) {
+    age--
+  }
+  if (
+    birthdate.getMonth() === now.getMonth() &&
+    now.getDate() < birthdate.getDate()
+  ) {
+    age--
+  }
+  return age
+}
